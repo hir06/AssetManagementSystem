9c950e3a995081afbabce2f925ab0158395e7461
Manish Kumar <m.kumar2@agilecockpit.com>
2018-01-04 12:19:51 +0530

    add vehicle form

--- src/app/components/equipment/add-equipment/add-equipment.component.ts
+++ src/app/components/equipment/add-equipment/add-equipment.component.ts
@@ -6,7 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./add-equipment.component.scss']
 })
 export class AddEquipmentComponent implements OnInit {
-
+  equipment: any = {};
   constructor() { }
 
   ngOnInit() {
--- src/app/components/vehicle/add-vehicle/add-vehicle.component.ts
+++ src/app/components/vehicle/add-vehicle/add-vehicle.component.ts
@@ -1,15 +1,13 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
 
 @Component({
-  selector: 'app-add-vehicle',
-  templateUrl: './add-vehicle.component.html',
-  styleUrls: ['./add-vehicle.component.scss']
+    selector: "app-add-vehicle",
+    templateUrl: "./add-vehicle.component.html",
+    styleUrls: ["./add-vehicle.component.scss"]
 })
 export class AddVehicleComponent implements OnInit {
+    vehicle: any = {};
+    constructor() {}
 
-  constructor() { }
-
-  ngOnInit() {
-  }
-
+    ngOnInit() {}
 }
