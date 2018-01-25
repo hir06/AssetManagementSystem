d24266bfa182649550c9067ded08a0200aafe7b7
Manish Kumar <m.kumar2@agilecockpit.com>
2018-01-25 15:34:43 +0530

    ajax loader for all http calls

--- src/app/services/api.services.ts
+++ src/app/services/api.services.ts
@@ -1,6 +1,10 @@
 import { AlertsLoaderService } from "./alerts-loader.service";
 import { Injectable } from "@angular/core";
-import { HttpClient, HttpHeaders } from "@angular/common/http";
+import { HttpClient, HttpHeaders, HttpErrorResponse } from "@angular/common/http";
+import { Observable } from "rxjs/Observable";
+import 'rxjs/add/operator/catch';
+import 'rxjs/add/observable/throw';
+
 
 @Injectable()
 export class ApiService {
@@ -16,13 +20,19 @@ export class ApiService {
             .map((res: any) => {
                 this._ajaxLoader.hideLoader();
                 return res;
+            }).catch((error: HttpErrorResponse) => {
+                  this._ajaxLoader.hideLoader();
+                  return Observable.throw(error);
             });
     }
     get(url: string) {
         this._ajaxLoader.showLoader();
-        return this._http.get(url).map((res: any) => {
+        return this._http.get(url) .map((res: any) => {
             this._ajaxLoader.hideLoader();
             return res;
+        }).catch((error: HttpErrorResponse) => {
+              this._ajaxLoader.hideLoader();
+              return Observable.throw(error);
         });
     }
 
