c5a2c367ddea8a7de32b36090303ff203dab5b22
Manish Kumar <m.kumar2@agilecockpit.com>
2018-01-29 17:19:02 +0530

    list dashboard of all 4 asset types

--- src/app/components/equipment/equipment-list/equipment-list.component.ts
+++ src/app/components/equipment/equipment-list/equipment-list.component.ts
@@ -1,17 +1,53 @@
-import { Component, OnInit } from '@angular/core';
+import { ApiService } from "./../../../services/api.services";
+import { Component, OnInit } from "@angular/core";
 
 @Component({
-  selector: 'app-equipment-list',
-  templateUrl: './equipment-list.component.html',
-  styleUrls: ['./equipment-list.component.scss']
+    selector: "app-equipment-list",
+    templateUrl: "./equipment-list.component.html",
+    styleUrls: ["./equipment-list.component.scss"]
 })
 export class EquipmentListComponent implements OnInit {
+    pageSize: number = 10;
+    itemsCount: number = 0;
+    equipmentList: any = [];
+    searchParams: any = {
+        paging: { currentPage: 0, pageSize: 10 },
+        sorts: [
+            {
+                field: "buildingName",
+                order: "ASC"
+            }
+        ],
+        filters: []
+    };
+    constructor(private _apiService: ApiService) {
+        this.getEquipmentList();
+    }
 
-  constructor() { }
+    ngOnInit() {}
 
-  ngOnInit() {
-  }
-  getPageData($event:any){
-    console.log($event);
-  }
+    getPageData($event: any) {
+        this.searchParams.paging.currentPage = $event.currentPage;
+        this.searchParams.paging.pageSize = $event.pageSize;
+        this.getEquipmentList();
+    }
+
+    getEquipmentList() {
+        let params = {
+            paging: { currentPage: 0, pageSize: 10 },
+            sorts: [
+                {
+                    field: "eqiupmentName",
+                    order: "ASC"
+                }
+            ],
+            filters: []
+        };
+        this._apiService
+            .get("/s/equipment/search-equipments", { Search: params })
+            .subscribe(data => {
+                this.itemsCount = data.totalRecords;
+                this.equipmentList = data.equipments;
+            });
+    }
 }
--- src/app/components/other/other-assets-list/other-assets-list.component.ts
+++ src/app/components/other/other-assets-list/other-assets-list.component.ts
@@ -1,18 +1,53 @@
-import { Component, OnInit } from '@angular/core';
+import { ApiService } from "./../../../services/api.services";
+import { Component, OnInit } from "@angular/core";
 
 @Component({
-  selector: 'app-other-assets-list',
-  templateUrl: './other-assets-list.component.html',
-  styleUrls: ['./other-assets-list.component.scss']
+    selector: "app-other-assets-list",
+    templateUrl: "./other-assets-list.component.html",
+    styleUrls: ["./other-assets-list.component.scss"]
 })
 export class OtherAssetsListComponent implements OnInit {
+    pageSize: number = 10;
+    itemsCount: number = 0;
+    otherAssetList: any = [];
+    searchParams: any = {
+        paging: { currentPage: 0, pageSize: 10 },
+        sorts: [
+            {
+                field: "buildingName",
+                order: "ASC"
+            }
+        ],
+        filters: []
+    };
+    constructor(private _apiService: ApiService) {
+        this.getOtherAssetList();
+    }
 
-  constructor() { }
+    ngOnInit() {}
 
-  ngOnInit() {
-  }
+    getPageData($event: any) {
+        this.searchParams.paging.currentPage = $event.currentPage;
+        this.searchParams.paging.pageSize = $event.pageSize;
+        this.getOtherAssetList();
+    }
 
-  getPageData($event:any){
-    console.log($event);
-  }
+    getOtherAssetList() {
+        let params = {
+            paging: { currentPage: 0, pageSize: 10 },
+            sorts: [
+                {
+                    field: "eqiupmentName",
+                    order: "ASC"
+                }
+            ],
+            filters: []
+        };
+        this._apiService
+            .get("/s/asset-type-other/search-asset-type-others", { Search: params })
+            .subscribe(data => {
+                this.itemsCount = data.totalRecords;
+                this.otherAssetList = data.assetTypeOthers;
+            });
+    }
 }
